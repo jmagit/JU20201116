1b183da9581f403601a4df242506da3b7434d133
Javier <jmartin@grupoloyal.com>
2020-11-16 23:52:51 +0100

    GildedRose

---
+++ GildedRose.stater/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GildedRoseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
